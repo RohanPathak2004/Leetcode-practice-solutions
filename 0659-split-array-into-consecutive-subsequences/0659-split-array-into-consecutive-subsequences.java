41b2894ebe533ba3c98953ca1d20de29035f8229
Rohan Pathak <155457626+RohanPathak2004@users.noreply.github.com>
2026-01-31 13:14:52 +0530

    Time: 48 ms (8.98%), Space: 50.2 MB (6.82%) - LeetHub

---
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean isPossible(int[] nums) {
+        HashMap<Integer,Integer> avlMap = new HashMap<>();
+        HashMap<Integer,Integer> vacMap = new HashMap<>();
+        for(int ele:nums){
+            avlMap.compute(ele,(k,v)->v==null?1:v+1);
+        }
+        for(int ele: nums){
+            int num = ele;
+            if(avlMap.get(num)==0) continue;
+             if(vacMap.containsKey(num)){
+                vacMap.put(num,vacMap.get(num)-1);
+                avlMap.put(num,avlMap.get(num)-1);
+                if(vacMap.get(num)==0) vacMap.remove(num);
+                vacMap.compute(num+1,(k,v)->v==null?1:v+1);
+                // if(avlMap.containsKey(num+1)&&avlMap.get(num+1)>0) {
+                //     avlMap.put(num+1,avlMap.get(num+1)-1);
+                // }
+            }
+            else if(avlMap.containsKey(num)&&avlMap.containsKey(num+1)&&avlMap.containsKey(num+2)&&avlMap.get(num)>0&&avlMap.get(num+1)>0&&avlMap.get(num+2)>0){
+                avlMap.put(num,avlMap.get(num)-1);
+                avlMap.put(num+1,avlMap.get(num+1)-1);
+                avlMap.put(num+2,avlMap.get(num+2)-1);
+                
+                vacMap.compute(num+3,(k,v)->v==null?1:v+1);
+
+            }else return false;
+            
+            
+        }
+        for(int ele:vacMap.keySet()){
+            System.out.println(ele+" "+vacMap.get(ele));
+        }
+        System.out.println(" ");
+        for(int ele:avlMap.keySet()){
+            System.out.println(ele+" "+avlMap.get(ele));
+            if(avlMap.get(ele)>=1) return false;
+        }
+        return true;
+    }
+}
