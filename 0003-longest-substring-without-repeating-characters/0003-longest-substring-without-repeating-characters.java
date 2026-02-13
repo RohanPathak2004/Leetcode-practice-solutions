e6d2c6eb2548f3e59a69a2bc27e93a6a37fca614
Rohan Pathak <155457626+RohanPathak2004@users.noreply.github.com>
2026-02-13 22:52:57 +0530

    Time: 5 ms (85.97%), Space: 44.6 MB (94.45%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,34 +1,26 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        if (n == 0)
-            return 0;
-        int i = 0;
-        int j = i;
-        int maxLen = 0;
+        // 2 pointer approach i and j sliding window
         HashMap<Character, Integer> map = new HashMap<>();
-        while (j < n) {
-            int len = 1;
+        int i = 0, j = 0;
+        int maxlen = 0;
+        while (j < s.length()) {
+            char ch = s.charAt(j);
+            if (map.containsKey(ch) && map.get(ch) >= i) {
+                int len = j - i;
+                maxlen = Math.max(maxlen, len);
+                while (s.charAt(i) != ch)
+                    i++;
+                i++;
 
-            while (j < n && !map.containsKey(s.charAt(j))) {
-                map.put(s.charAt(j), j);
-                j++;
             }
-            len = j - i;
-            maxLen = Math.max(len, maxLen);
-            if (j < n && map.containsKey(s.charAt(j))) {
-                i = map.get(s.charAt(j)) + 1;
-            }
-
-            Iterator<Character> iterator = map.keySet().iterator();
-
-            while (iterator.hasNext()) {
-                Character ch = iterator.next();
-                if (map.get(ch) < i)
-                    iterator.remove();
-            }
-
+                
+            map.put(ch, j);
+            
+            j++;
         }
-        return maxLen;
+        int len = j - i;
+        maxlen = Math.max(maxlen, len);
+        return maxlen;
     }
 }
