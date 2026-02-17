644c9ad6e2545f1baa150f1e87ac3bf20f4788bd
Rohan Pathak <155457626+RohanPathak2004@users.noreply.github.com>
2026-02-17 22:30:13 +0530

    Time: 65 ms (6.44%), Space: 47 MB (28.42%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,31 +1,31 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         int n = s.length();
-        HashMap<Character,Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
         int i = 0;
         int j = 0;
         int maxW = 0;
-        while(i<n&&j<n){
+        while (i < n && j < n) {
             char ch = s.charAt(j);
-            map.compute(ch,(key,val)->val==null?1:val+1);
-            int window = j-i+1;
+            map.compute(ch, (key, val) -> val == null ? 1 : val + 1);
+            int window = j - i + 1;
             int freq = 0;
-            for(char c:map.keySet()){
-                freq = Math.max(freq,map.get(c));
+            for (char c : map.keySet()) {
+                freq = Math.max(freq, map.get(c));
             }
-            
-            while(i<n&&(window-freq)>k){
-                
-                map.put(s.charAt(i),map.get(s.charAt(i))-1);
+
+            while (i < n && (window - freq) > k) {
+
+                map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
                 i++;
-                window = j-i+1;
+                window = j - i + 1;
             }
-            window = j-i+1;
-            maxW = Math.max(window,maxW);
+            window = j - i + 1;
+            maxW = Math.max(window, maxW);
             j++;
         }
-        int window = j-i;
-        maxW = Math.max(window,maxW);
+        int window = j - i;
+        maxW = Math.max(window, maxW);
         return maxW;
     }
 }
