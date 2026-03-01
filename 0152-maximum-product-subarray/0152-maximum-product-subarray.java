5d8020679c7dd5c4b7091f68e39023f6e585caa1
Rohan Pathak <155457626+RohanPathak2004@users.noreply.github.com>
2026-03-01 23:56:31 +0530

    Time: 0 ms (100%), Space: 47.2 MB (73.26%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,26 +1,26 @@
 class Solution {
     public int maxProduct(int[] nums) {
         int n = nums.length;
-        int[] pre = new int[n];
-        int[] suf = new int[n];
         int maxPro = Integer.MIN_VALUE;
-        pre[0] = nums[0];
-        for (int i = 1; i < n ; i++) {
-            if (nums[i] != 0)
-                pre[i] = nums[i] * pre[i - 1];
-            if (pre[i - 1] == 0)
-                pre[i] = nums[i];
-
+        int currPro = 1;
+        for(int i = 0 ; i<n ;i++){
+            if(nums[i]==0){
+                maxPro = Math.max(0,maxPro);
+                currPro = 1;
+            }else{
+                currPro = currPro*nums[i];
+                maxPro = Math.max(currPro,maxPro);
+            }
         }
-        suf[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] != 0)
-                suf[i] = suf[i + 1] * nums[i];
-            if (suf[i + 1] == 0)
-                suf[i] = nums[i];
-        }
-        for (int i = 0; i < n; i++) {
-            maxPro = Math.max(maxPro, Math.max(pre[i], suf[i]));
+        currPro = 1;
+        for(int i = n-1 ; i>=0 ;i--){
+            if(nums[i]==0){
+                maxPro = Math.max(0,maxPro);
+                currPro = 1;
+            }else{
+                currPro = currPro*nums[i];
+                maxPro = Math.max(currPro,maxPro);
+            }
         }
         return maxPro;
     }
