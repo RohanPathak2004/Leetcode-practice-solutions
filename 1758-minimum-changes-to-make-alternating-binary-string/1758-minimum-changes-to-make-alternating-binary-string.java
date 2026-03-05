516f7432fcb258396ceb1e0371d39cd8a4a12015
Rohan Pathak <155457626+RohanPathak2004@users.noreply.github.com>
2026-03-05 23:56:37 +0530

    Time: 65 ms (5.34%), Space: 47.2 MB (5.34%) - LeetHub

---
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String createString(char ch,int n){
+        String s = ch+"";
+        for(int i = 1 ;i<n ; i++){
+            if(s.charAt(i-1)=='1'){
+                s+='0';
+            }else{
+                s+='1';
+            }
+        }
+        return s;
+    }
+    public int minOperations(String s) {
+        int n = s.length();
+        if(n==1) return 0;
+        char ch = s.charAt(0);
+        String one = createString(ch,n);
+        String zero = createString(ch=='1'?'0':'1',n);
+        // System.out.println(one+" "+zero);
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for(int i = 0; i<n ; i++){
+            if(s.charAt(i)!=one.charAt(i)) cnt1++;
+            if(s.charAt(i)!=zero.charAt(i)) cnt2++;
+        }
+
+        return Math.min(cnt1,cnt2);
+    }
+}
