a261490521ebb948370dc2684750f8773efb4ad3
Rohan Pathak <155457626+RohanPathak2004@users.noreply.github.com>
2026-03-18 15:55:18 +0530

    Time: 11 ms (17.48%), Space: 50.9 MB (5.79%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,18 +1,37 @@
 class NumArray {
+    int[] tree;
+    int n;
 
-    int[] pre ;
-    public NumArray(int[] nums) {
-        int n = nums.length;
-        pre = new int[n];
-        pre[0] = nums[0];
-        for(int i = 1; i<n ; i++){
-            pre[i]+=pre[i-1]+nums[i];
-        }
+    public int buildTree(int i, int l, int r, int[] tree, int[] arr) {
+        if (l == r)
+            return tree[i] = arr[l];
+        int mid = (l + r) / 2;
+        int left = buildTree(i * 2 + 1, l, mid, tree, arr);
+        int right = buildTree(i * 2 + 2, mid + 1, r, tree, arr);
+        return tree[i] = left + right;
     }
-    
+
+    public NumArray(int[] nums) {
+        n = nums.length;
+        tree = new int[4 * n];
+        int totalSum = buildTree(0, 0, n - 1, tree, nums);
+        // System.out.println(totalSum);
+    }
+
+    public int query(int i, int l, int r, int L, int R) {
+        if (r < L || l > R)
+            return 0;
+
+        if (l >= L && r <= R)
+            return tree[i];
+
+        int mid = (l + r) / 2;
+        return query(2 * i + 1, l, mid, L, R) +
+                query(2 * i + 2, mid + 1, r, L, R);
+    }
+
     public int sumRange(int left, int right) {
-        if(left==0) return pre[right];
-        return pre[right] - pre[left-1];
+        return query(0, 0, n - 1, left, right);
     }
 }
 
