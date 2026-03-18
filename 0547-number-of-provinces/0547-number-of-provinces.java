3330700babe28447656fc928da1f2223b5630f1a
Rohan Pathak <155457626+RohanPathak2004@users.noreply.github.com>
2026-03-18 22:42:40 +0530

    Time: 1 ms (87.75%), Space: 47.2 MB (69.55%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,25 +1,27 @@
 class Solution {
-    public int findCircleNum(int[][] grh) {
-        int n = grh.length;
-        boolean[] visited = new boolean[n];
-        Queue<Integer> q = new LinkedList<>();
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                count++;
-                q.add(i);
-                while (q.size() != 0) {
-                    int node = q.remove();
-                    for (int j = 0; j < n; j++) {
-                        if (grh[node][j] == 1 && !visited[j]) {
-                            // System.out.println(j);
-                            q.add(j);
-                            visited[j] = true;
-                        }
-                    }
-                }
+
+    public void explore(int i,boolean[] visited,int[][] adj){
+        if(visited[i]) return ;
+        visited[i] = true;
+        for(int j = 0 ;j<adj.length ; j++){
+            if(adj[i][j]==1){
+                
+                explore(j,visited,adj);
             }
         }
+    }
+
+    public int findCircleNum(int[][] adj) {
+        int n = adj.length;
+        boolean[] visited = new boolean[n];
+        int count = 0;
+        for(int i = 0; i<n ;i++){
+            if(!visited[i]){
+                count++;
+                explore(i,visited,adj);
+            }
+        }
+
         return count;
     }
 }
