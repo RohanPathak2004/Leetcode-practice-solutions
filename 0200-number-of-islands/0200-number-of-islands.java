d7df9f2d7e4218add4e73adedfd1583b9a2468df
Rohan Pathak <155457626+RohanPathak2004@users.noreply.github.com>
2026-03-18 23:18:39 +0530

    Time: 4 ms (45.23%), Space: 52 MB (75.15%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,86 +1,27 @@
 class Solution {
-    class Cell {
-        int x;
-        int y;
-        public Cell(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o)
-                return true;
-            if (!(o instanceof Cell))
-                return false;
-            Cell cell = (Cell) o;
-            return x == cell.x && y == cell.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
+    public void dfs(int i,int j,int m,int n,boolean[][] visited,char[][] grid){
+        if(i<0||i>=m||j<0||j>=n||visited[i][j]||grid[i][j]=='0') return;
+        visited[i][j] = true;
+        dfs(i-1,j,m,n,visited,grid);
+        dfs(i+1,j,m,n,visited,grid);
+        dfs(i,j-1,m,n,visited,grid);
+        dfs(i,j+1,m,n,visited,grid);
     }
-
     public int numIslands(char[][] grid) {
-        HashMap<Cell, Set<Cell>> map = new HashMap<>();
         int m = grid.length;
         int n = grid[0].length;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    
-                    Cell c = new Cell(i, j);
-                    map.putIfAbsent(c, new HashSet<>());
-                    if (i - 1 >= 0 && grid[i - 1][j] == '1') {
-                        Cell up = new Cell(i - 1, j);
-                        map.computeIfAbsent(up, k -> new HashSet<>()).add(c);
-                        map.computeIfAbsent(c, k -> new HashSet<>()).add(up);
-                    }
-                    if (i + 1 < m && grid[i + 1][j] == '1') {
-                        Cell down = new Cell(i + 1, j);
-                        map.computeIfAbsent(down, k -> new HashSet<>()).add(c);
-                        map.computeIfAbsent(c, k -> new HashSet<>()).add(down);
-                    }
-                    if (j - 1 >= 0 && grid[i][j - 1] == '1') {
-                        Cell left = new Cell(i, j - 1);
-                        map.computeIfAbsent(left, k -> new HashSet<>()).add(c);
-                        map.computeIfAbsent(c, k -> new HashSet<>()).add(left);
-                    }
-                    if (j + 1 < n && grid[i][j + 1] == '1') {
-                        Cell right = new Cell(i, j + 1);
-                        map.computeIfAbsent(right, k -> new HashSet<>()).add(c);
-                        map.computeIfAbsent(c, k -> new HashSet<>()).add(right);
-                    }
-                }
-            }
-        }
         boolean[][] visited = new boolean[m][n];
-        Queue<Cell> q = new LinkedList<>();
-        int ans = 0;
+        int count = 0;
         for(int i = 0; i<m ; i++){
-            for(int j = 0; j<n ; j++){
+            for(int j = 0; j<n ;j++){
                 if(grid[i][j]=='1'&&!visited[i][j]){
-                    ans++;
-                    q.add(new Cell(i,j));
-                    while(q.size()!=0){
-                        Cell front = q.remove();
-                        Set<Cell> s = map.get(front);
-                        if(s==null) break;
-                        for(Cell c: s){
-                            if(!visited[c.x][c.y]){
-                                 q.add(c);
-                                 visited[c.x][c.y] = true;
-                            }
-
-                        }
-                    }
+                    count++;
+                    dfs(i,j,m,n,visited,grid);
+                    // System.out.println(visited[i][j]);
                 }
             }
         }
 
-        return ans;
+        return count;
     }
 }
