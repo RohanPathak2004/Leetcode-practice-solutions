d9788e9b2456c1c5f6302c0ad6b38f2fe2b07c42
Rohan Pathak <155457626+RohanPathak2004@users.noreply.github.com>
2026-03-26 14:56:54 +0530

    Time: 10 ms (53.85%), Space: 47 MB (21.24%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -2,15 +2,14 @@ class Solution {
     public int rec(int i, int cur, int tar, int[] nums, int[][] dp){
         if(i>=nums.length&&cur==tar) return 1;
         if(i>=nums.length) return 0;
-        if(dp[i][Math.abs(cur+1000)]!=Integer.MIN_VALUE) return dp[i][Math.abs(cur+1000)];
+        if(dp[i][cur+1000]!=Integer.MIN_VALUE) return dp[i][cur+1000];
         // int skip = rec(i+1, cur, tar, nums, dp);
         int pickPos = rec(i+1, cur+nums[i], tar, nums, dp);
         int pickNeg = rec(i+1, cur-nums[i], tar, nums, dp);
-        int ans ;
-        if(pickPos>=0&&pickNeg>=0) ans = pickPos+pickNeg;
-        else if(pickPos>=0) ans = pickPos;
-        else ans = pickNeg;
-        return dp[i][Math.abs(cur+1000)] = ans;
+        // int ans =0 ;
+        
+        
+        return dp[i][cur+1000] = pickPos+pickNeg;
     }
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
