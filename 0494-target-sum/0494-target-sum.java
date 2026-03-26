c13a88d884264963737818e2ef1fc1af0a3171ad
Rohan Pathak <155457626+RohanPathak2004@users.noreply.github.com>
2026-03-26 15:20:14 +0530

    Time: 89 ms (41.33%), Space: 47.5 MB (5.24%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,21 +1,43 @@
 class Solution {
-    public int rec(int i, int cur, int tar, int[] nums, int[][] dp){
+    public class Pair{
+        int idx;
+        int sum ;
+        public Pair(int idx, int sum){
+            this.idx = idx;
+            this.sum = sum;
+        }
+        @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair pair = (Pair) o;
+        return idx == pair.idx && sum == pair.sum;
+    }
+
+    @Override
+    public int hashCode() {
+        // Objects.hash is a simple way to combine fields into a hash
+        return Objects.hash(idx, sum);
+    }
+
+    }
+    public int rec(int i, int cur, int tar, int[] nums, HashMap<Pair,Integer> map){
         if(i>=nums.length&&cur==tar) return 1;
         if(i>=nums.length) return 0;
-        if(dp[i][cur+1000]!=Integer.MIN_VALUE) return dp[i][cur+1000];
+        Pair p = new Pair(i, cur);
+        if(map.containsKey(p)) return map.get(p);
         // int skip = rec(i+1, cur, tar, nums, dp);
-        int pickPos = rec(i+1, cur+nums[i], tar, nums, dp);
-        int pickNeg = rec(i+1, cur-nums[i], tar, nums, dp);
+        int pickPos = rec(i+1, cur+nums[i], tar, nums, map);
+        int pickNeg = rec(i+1, cur-nums[i], tar, nums, map);
         // int ans =0 ;
-        
-        
-        return dp[i][cur+1000] = pickPos+pickNeg;
+        int ans  = pickPos+pickNeg;
+        map.put(p,ans);
+        return ans;
     }
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
-        int[][] dp = new int[n][2*1000+1];
-        for(int[] row: dp) Arrays.fill(row,Integer.MIN_VALUE);
-        int ans =  rec(0, 0, target, nums, dp);
+        HashMap<Pair,Integer> map = new HashMap<>();
+        int ans =  rec(0, 0, target, nums, map);
         return ans;
     }
 }
