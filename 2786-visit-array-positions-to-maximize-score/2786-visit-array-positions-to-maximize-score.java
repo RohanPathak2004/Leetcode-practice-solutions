6a16fe15fa0642e9390b2a1e5cff9b500f778ed7
Rohan Pathak <155457626+RohanPathak2004@users.noreply.github.com>
2026-03-27 13:42:20 +0530

    Time: 59 ms (27.74%), Space: 107.9 MB (22.63%) - LeetHub

--- 2786-visit-array-positions-to-maximize-score/2786-visit-array-positions-to-maximize-score.java
+++ 2786-visit-array-positions-to-maximize-score/2786-visit-array-positions-to-maximize-score.java
@@ -1,52 +1,47 @@
 class Solution {
-    class Pair {
-        int idx;
-        boolean iseven;
+    // class Pair {
+    //     int idx;
+    //     boolean iseven;
 
-        public Pair(int idx, boolean iseven) {
-            this.idx = idx;
-            this.iseven = iseven;
-        }
+    //     public Pair(int idx, boolean iseven) {
+    //         this.idx = idx;
+    //         this.iseven = iseven;
+    //     }
 
-        @Override
-        public boolean equals(Object o) {
-            Pair p = (Pair) o;
-            return idx == p.idx && iseven == p.iseven;
-        }
+    //     @Override
+    //     public boolean equals(Object o) {
+    //         Pair p = (Pair) o;
+    //         return idx == p.idx && iseven == p.iseven;
+    //     }
 
-        @Override
-        public int hashCode() {
-            return Objects.hash(idx, iseven);
-        }
-    }
+    //     @Override
+    //     public int hashCode() {
+    //         return Objects.hash(idx, iseven);
+    //     }
+    // }
 
-    public long rec(int i, boolean evenParaty, int x, int n, int[] nums, HashMap<Pair, Long> dp) {
+    public long rec(int i, int evenParaty, int x, int n, int[] nums, long[][] dp) {
         if (i >= n)
             return 0;
-        Pair p = new Pair(i, evenParaty);
-        if (dp.containsKey(p)) {
-            // System.out.println("working...");
-            return dp.get(p);
-        }
+        if(dp[i][evenParaty]!=-1) return dp[i][evenParaty];
         long skip = rec(i + 1, evenParaty, x, n, nums, dp);
         
-        long pick = nums[i] + rec(i + 1, nums[i]%2==0, x, n, nums, dp);
-        if ((nums[i] % 2==0) != evenParaty) {
+        long pick = nums[i] + rec(i + 1, nums[i]%2, x, n, nums, dp);
+        if (nums[i] % 2 != evenParaty) {
             pick = pick - x;
         }
 
-        long ans = Math.max(skip, pick);
-        dp.put(new Pair(i, evenParaty), ans);
-        return ans;
+        return dp[i][evenParaty] = Math.max(skip, pick);
+        
     }
 
     public long maxScore(int[] nums, int x) {
         int n = nums.length;
         // long total = Arrays.stream(nums).reduce(0,(a,b)->a+b);
         // long[][] dp = new long[n][n];
-        HashMap<Pair, Long> dp = new HashMap<>();
-        // for (long[] row : dp)
-        //     Arrays.fill(row, -1);
-        return nums[0] + rec(1, nums[0]%2==0, x, n, nums, dp);
+        long[][] dp = new long[n][2];
+        for (long[] row : dp)
+            Arrays.fill(row, -1);
+        return nums[0] + rec(1, nums[0]%2, x, n, nums, dp);
     }
 }
