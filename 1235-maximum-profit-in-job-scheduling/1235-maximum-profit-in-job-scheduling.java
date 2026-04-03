ef8a29544007d7124a3458b9ee329c3620603629
Rohan Pathak <155457626+RohanPathak2004@users.noreply.github.com>
2026-04-03 16:19:06 +0530

    Time: 47 ms (32.76%), Space: 83.7 MB (8.26%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -22,17 +22,17 @@ class Solution {
 
         return l;
     }
-    public int rec(int i, int n, int maxPoint, Job[] arr, HashMap<Integer,Integer> dp) {
+    public int rec(int i, int n, Job[] arr, HashMap<Integer,Integer> dp) {
         if (i >= n)
             return 0;
 
         if(dp.containsKey(i)) return dp.get(i);
 
-        int skip = rec(i + 1, n, maxPoint, arr, dp);
+        int skip = rec(i + 1, n, arr, dp);
 
         int pick = arr[i].p;
         int nextIdx = lowerBound(i, n, arr[i].e, arr);
-        pick += rec(nextIdx,n,maxPoint,arr,dp);
+        pick += rec(nextIdx,n,arr,dp);
         int ans =  Math.max(pick,skip);
 
         dp.put(i,ans);
@@ -44,12 +44,11 @@ class Solution {
         int maxPoint = -1;
         for (int i = 0; i < n; i++) {
             arr[i] = new Job(s[i], e[i], p[i]);
-            maxPoint = Math.max(maxPoint,s[i]);
         }
 
         Arrays.sort(arr, (a, b) -> a.s - b.s);
         HashMap<Integer,Integer> dp = new HashMap<>();
-        
-        return rec(0, n, maxPoint, arr, dp);
+
+        return rec(0, n, arr, dp);
     }
 }
