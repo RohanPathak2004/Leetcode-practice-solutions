f5620cee55949fd59d57711ae463b539a214a85d
Rohan Pathak <155457626+RohanPathak2004@users.noreply.github.com>
2026-04-03 16:11:42 +0530

    Time: 45 ms (33.88%), Space: 83.4 MB (8.51%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -10,9 +10,7 @@ class Solution {
             this.p = p;
         }
     }
-
-    public int lowerBound(int l, int h, int tar, Job[] arr) {
-        
+    public int lowerBound(int l, int h, int tar, Job[] arr) {    
         while (l < h) {
             int mid = l + (h - l) / 2;
             if (arr[mid].s < tar)
@@ -24,25 +22,22 @@ class Solution {
 
         return l;
     }
-
     public int rec(int i, int n, int maxPoint, Job[] arr, HashMap<Integer,Integer> dp) {
         if (i >= n)
             return 0;
+
         if(dp.containsKey(i)) return dp.get(i);
-            
+
         int skip = rec(i + 1, n, maxPoint, arr, dp);
+
         int pick = arr[i].p;
-        // if(arr[i].e>maxPoint) return pick;
         int nextIdx = lowerBound(i, n, arr[i].e, arr);
-        // System.out.println(nextIdx);
-        // if (nextIdx >= n)
-        //     return arr[i].p;
         pick += rec(nextIdx,n,maxPoint,arr,dp);
         int ans =  Math.max(pick,skip);
+
         dp.put(i,ans);
         return ans;
     }
-
     public int jobScheduling(int[] s, int[] e, int[] p) {
         int n = s.length;
         Job[] arr = new Job[n];
@@ -51,13 +46,10 @@ class Solution {
             arr[i] = new Job(s[i], e[i], p[i]);
             maxPoint = Math.max(maxPoint,s[i]);
         }
+
         Arrays.sort(arr, (a, b) -> a.s - b.s);
-        // for(Job j: arr) System.out.println(j.s+" "+j.e+" "+j.p+" ");
         HashMap<Integer,Integer> dp = new HashMap<>();
-        int ans = -1;
         
-            ans = Math.max(ans, rec(0, n, maxPoint, arr, dp));
-        
-        return ans;
+        return rec(0, n, maxPoint, arr, dp);
     }
 }
