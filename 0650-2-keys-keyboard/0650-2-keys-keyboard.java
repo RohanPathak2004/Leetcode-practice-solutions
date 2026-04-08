e8fd7a7b3ab292009d2c76da794234eabeb5a92a
Rohan Pathak <155457626+RohanPathak2004@users.noreply.github.com>
2026-04-08 13:43:12 +0530

    Time: 25 ms (19%), Space: 200.2 MB (5.02%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,13 +1,16 @@
 class Solution {
-    public long rec(int dis, int pas, int n){
+    public long rec(int dis, int pas, int n,long[][] dp){
         if(dis==n) return 0;
         if(dis>n) return Integer.MAX_VALUE;
-        long p = 1+rec(dis+pas,pas,n);
-        long pc = 2 + rec(dis+pas,dis+pas, n);
-        return Math.min(p,pc);
+        if(dp[dis][pas]!=Integer.MAX_VALUE) return dp[dis][pas];
+        long p = 1+rec(dis+pas,pas,n,dp);
+        long pc = 2 + rec(dis+pas,dis+pas, n,dp);
+        return dp[dis][pas] =  Math.min(p,pc);
     }
     public int minSteps(int n) {
         if(n==1) return 0;
-        return 1 + (int)rec(1,1,n);
+        long[][] dp = new long[n+1][n+1];
+        for(long[] r: dp)  Arrays.fill(r,Integer.MAX_VALUE);
+        return 1 + (int)rec(1,1,n,dp);
     }
 }
