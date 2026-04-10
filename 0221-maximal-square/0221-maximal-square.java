98d1965f482f93c86fdfae44591a81e94f81eee6
Rohan Pathak <155457626+RohanPathak2004@users.noreply.github.com>
2026-04-10 14:23:49 +0530

    Time: 5 ms (99.85%), Space: 71 MB (62.73%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -5,7 +5,7 @@ class Solution {
         int[][] matrix = new int[m][n];
         for(int i = 0; i<m ;i++){
             for(int j = 0; j<n ;j++){
-                matrix[i][j] = Integer.parseInt(arr[i][j]+"");
+                matrix[i][j] = (int)(arr[i][j]-48);
             }
         }
         for(int i = 1 ; i<m ; i++){
