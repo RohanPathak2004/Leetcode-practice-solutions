22a60d31e13eb400d33213dc24c5cac37f1f69ce
Rohan Pathak <155457626+RohanPathak2004@users.noreply.github.com>
2026-04-15 15:20:40 +0530

    Time: 17 ms (8.28%), Space: 47.4 MB (12.14%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int rec(int i, int n, int[] nums, int[] freq,int[] dp){
+        if(i>=n){
+            return 0;
+        }
+        if(dp[i]!=-1) return dp[i];
+        int skip = rec(i+1,n,nums,freq,dp);
+        int point = nums[i]*freq[i];
+        int idx = 0;
+        if(i+1<n&&(nums[i+1]==nums[i]+1)) idx = i+2;
+        else idx = i+1;
+        int pick = point+rec(idx,n,nums,freq,dp);
+        // System.out.println(skip+" "+pick);
+        return dp[i] = Math.max(skip,pick);
+    }
+    public int deleteAndEarn(int[] nums) {
+        HashMap<Integer, Integer> map = new LinkedHashMap<>();
+        for(int ele: nums) map.compute(ele,(k,v)->v==null?1:v+1);
+        int n = map.size();
+        int[] arr = new int[n];
+        int[] freq = new int[n];
+        Arrays.sort(nums);
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1);
+        int idx = 0;
+        for(int i = 0; i<nums.length ; i++){
+            if(map.containsKey(nums[i])){
+                arr[idx] = nums[i];
+                freq[idx] = map.get(nums[i]);
+                map.remove(nums[i]);
+                idx++;
+            }
+        }
+        
+        return rec(0,n,arr,freq,dp);
+    }
+}
