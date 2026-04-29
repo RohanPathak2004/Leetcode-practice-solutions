5c4dd90b3fba7325bd4a1a20efa11dff29841356
Rohan Pathak <155457626+RohanPathak2004@users.noreply.github.com>
2026-04-29 13:59:50 +0530

    Time: 1 ms (81.19%), Space: 45.7 MB (84.86%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.java
+++ 0808-soup-servings/0808-soup-servings.java
@@ -1,22 +1,22 @@
 class Solution {
     public double rec(int a,int b,double[][] dp){
-        if(a<=0||b<=0){
-            if(a<=0&&a==b) return 0.5;
-            if((a<=0&&b>a)) return 1;
-            if((a>0&&b<=0)||(a<=0&&b<a)) return 0;
-        }
+        if(a<=0&&b<=0) return 0.5;
+        if(a<=0) return 1.0;
+        if(b<=0) return 0.0;
         // System.out.println(a+" "+b);
         if(dp[a][b]!=-1) return dp[a][b];
-        double op1 = rec(a-100,b,dp);
-        double op2 = rec(a-75,b-25,dp);
-        double op3 = rec(a-50,b-50,dp);
-        double op4 = rec(a-25,b-75,dp);
+        double op1 = rec(a-4,b,dp);
+        double op2 = rec(a-3,b-1,dp);
+        double op3 = rec(a-2,b-2,dp);
+        double op4 = rec(a-1,b-3,dp);
         return dp[a][b] = 0.25*(op1+op2+op3+op4);
     }
     public double soupServings(int n) {
         if(n>=4800) return 1; // threshold 
-        double[][] dp = new double[n+1][n+1];
+        int units = (int)Math.ceil(n/25.0);
+        double[][] dp = new double[units+1][units+1];
+
         for(double[] c: dp) Arrays.fill(c,-1);
-        return rec(n,n,dp);
+        return rec(units,units,dp);
     }
 }
