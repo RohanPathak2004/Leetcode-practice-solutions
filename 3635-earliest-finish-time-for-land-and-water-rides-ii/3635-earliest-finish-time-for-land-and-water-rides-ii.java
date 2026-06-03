2d1f55bcc0b5e680cd73f7d4fe9a827f667ad20e
Rohan Pathak <155457626+RohanPathak2004@users.noreply.github.com>
2026-06-03 13:44:44 +0530

    Time: 3 ms (50%), Space: 101 MB (8.62%) - LeetHub

--- 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.java
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.java
@@ -2,28 +2,28 @@ class Solution {
     public int earliestFinishTime(int[] lst, int[] ld, int[] wst, int[] wd) {
         int n = lst.length;
         int m = wst.length;
-        int[][] l = new int[n][2];
-        int[][] w = new int[m][2];
-        for (int i = 0; i < n; i++) {
-            l[i][0] = lst[i];
-            l[i][1] = ld[i];
-        }
-        for (int i = 0; i < m; i++) {
-            w[i][0] = wst[i];
-            w[i][1] = wd[i];
-        }
+        // int[][] l = new int[n][2];
+        // int[][] w = new int[m][2];
+        // for (int i = 0; i < n; i++) {
+        //     l[i][0] = lst[i];
+        //     l[i][1] = ld[i];
+        // }
+        // for (int i = 0; i < m; i++) {
+        //     w[i][0] = wst[i];
+        //     w[i][1] = wd[i];
+        // }
         
         int minT = Integer.MAX_VALUE;
         int min = Integer.MAX_VALUE;
         for(int i = 0; i<n ; i++){
-            min = Math.min(min,l[i][0]+l[i][1]);
+            min = Math.min(min,lst[i]+ld[i]);
         }
         for(int i = 0; i<m ; i++){
             int t = min;
-            if (t < w[i][0]) {
-                t += (w[i][0] - t) + w[i][1];
+            if (t < wst[i]) {
+                t += (wst[i] - t) + wd[i];
             } else {
-                t += w[i][1];
+                t += wd[i];
             }
             minT = Math.min(minT, t);
         }
@@ -31,14 +31,14 @@ class Solution {
 
         min = Integer.MAX_VALUE;
         for(int i = 0; i<m ; i++){
-            min = Math.min(min,w[i][0]+w[i][1]);
+            min = Math.min(min,wst[i]+wd[i]);
         }
         for (int i = 0; i < n; i++) {
             int t = min;
-            if (t < l[i][0]) {
-                t += (l[i][0] - t) + l[i][1];
+            if (t < lst[i]) {
+                t += (lst[i] - t) + ld[i];
             } else {
-                t += l[i][1];
+                t += ld[i];
             }
             minT = Math.min(minT,t);
         }
